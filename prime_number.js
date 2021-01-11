ea03c33734035b713b24f5cd95df17b561432c38
Mtwod <maximewong@hotmail.com>
2021-01-11 18:03:26 +0100

    Optimize function

--- prime_number.js
+++ prime_number.js
@@ -1,9 +1,9 @@
 // Return true if number is a prime number, else otherwise
-const isPrimeNumber = (n, divisor = n-1) => {
+const isPrimeNumber = (n, divisor = n-2) => {
   if (n < 2) return 0;
-  if (divisor === 1) return true;
-  if (n % divisor === 0) return false;
-  return isPrimeNumber(n, divisor - 1);
+  if (n === 2 || divisor === 1) return true;
+  if (n % 2 === 0 || n % divisor === 0) return false;
+  return isPrimeNumber(n, divisor - 2);
 }
 
 console.log(isPrimeNumber(-1));
@@ -13,3 +13,4 @@ console.log(isPrimeNumber(50));
 console.log(isPrimeNumber(51));
 console.log(isPrimeNumber(13));
 console.log(isPrimeNumber(43));
+console.log(isPrimeNumber(2));
